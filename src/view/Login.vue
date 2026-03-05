d77a2540b9b4927572b95b81dc06075764a390de
yaohui.li <liyaohuiisgreat@gmail.com>
2026-03-05 22:59:43 +0800

    添加登录卡片和用户名密码输入表单。

--- src/view/Login.vue
+++ src/view/Login.vue
@@ -1,8 +1,44 @@
 <script setup>
+    import { reactive } from 'vue';
+
+    const loginInfo = reactive({
+        username: '',
+        password: '',
+    })
 </script>
 
 <template>  
-    <div>
-        login
-    </div>
+  <el-card style="max-width: 480px">
+    <!-- 标题 -->
+    <h2>kubernetes Manager</h2>
+    <!-- 表单 -->
+    <el-form
+        ref="ruleFormRef"
+        style="max-width: 600px"
+        :model="loginInfo"
+        status-icon
+        :rules="rules"
+        label-width="auto"
+        class="demo-ruleForm"
+    >
+        <!-- 用户名 -->
+        <el-form-item prop="checkPass">
+        <el-input
+            v-model="loginInfo.username"
+        />
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="pass">
+        <el-input v-model="loginInfo.password" type="password" />
+        </el-form-item>
+        <!-- 登录 -->
+        <el-form-item>
+        <el-button type="primary" @click="submitForm(ruleFormRef)">
+            登录
+        </el-button>
+        </el-form-item>
+    </el-form>    
+
+  </el-card>
+  
 </template>
