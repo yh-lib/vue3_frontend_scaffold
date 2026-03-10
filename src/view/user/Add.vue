c32b505997dc583060cd5d960597eda74c554b02
yaohui.li <liyaohuiisgreat@gmail.com>
2026-03-11 04:21:57 +0800

    表单重置功能已完成

--- src/view/user/Add.vue
+++ src/view/user/Add.vue
@@ -1,38 +1,44 @@
 <script lang="ts" setup>
 import { reactive, ref } from 'vue'
-import type { FormInstance, FormRules } from 'element-plus'
-
+import type { FormInstance } from 'element-plus'
+// 定义用户属性
 const userForm = reactive({
     username:"",
     qq:"",
     address:"",
     id:""
 })
-
+// 重置表单
+const userFormRef = ref()
+const resetForm = () => {
+    userFormRef.value.resetFields()
+}
 </script>
 
 <template>
   <el-form
     ref="userFormRef"
     :model="userForm"
+    label-width="64px"
     center
   >
-    <el-form-item label="姓名" label-width="64px">
+    <!-- 表单 -->
+    <el-form-item label="姓名"  prop="username">
       <el-input v-model="userForm.username" autocomplete="off" />
     </el-form-item>
-    <el-form-item label="ID" label-width="64px">
+    <el-form-item label="ID"  prop="id">
       <el-input v-model="userForm.id" autocomplete="off" />
     </el-form-item>
-    <el-form-item label="QQ" label-width="64px">
+    <el-form-item label="QQ"  prop="qq">
       <el-input v-model="userForm.qq" autocomplete="off" />
     </el-form-item>
-    <el-form-item label="地址" label-width="64px">
+    <el-form-item label="地址"  prop="address">
       <el-input v-model="userForm.address" autocomplete="off" />
     </el-form-item>
-
+    <!-- 按钮 -->
     <el-form-item>
         <el-button type="primary" @click="submitForm(userForm)" style="margin: 0 auto 0 30%;">提交</el-button>
-        <el-button @click="resetForm(userForm)" style="margin: 0 30% 0 auto;">重置</el-button>
+        <el-button @click="resetForm()" style="margin: 0 30% 0 auto;">重置</el-button>
     </el-form-item>
   </el-form>
 </template>
